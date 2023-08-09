f276cf44e589e17c15abdf929417956cd23db1d3
YarinNaftali <78485725+YarinNaftali@users.noreply.github.com>
2023-08-09 16:21:51 +0300

    Decoupled the API level with the internal logic.
    Now everything is inside the Product model.
    Added Logging as well for those operations.

--- src/api/v1/product/product_endpoint.py
+++ src/api/v1/product/product_endpoint.py
@@ -6,21 +6,17 @@ router = APIRouter(prefix="/product")
 
 @router.get("/")
 async def get_products():
-    return await Product.find_all().to_list()
+    return await Product.get_products()
 
 @router.post("/")
 async def create_product(product: Product):
-    return await product.insert()
+    return await product.create_product()
 
 @router.get("/{product_id}")
 async def get_product(product_id: UUID):
     return await Product.find_one(product_id)
 
-@router.put("/{product_id}")
-async def update_product(product_id: UUID, product: Product):
-    return await Product.replace_one(product_id, product)
-
 @router.delete("/{product_id}")
 async def delete_product(product_id: UUID):
-    return await Product.find_one(product_id).delete()
+    return await Product.delete_product(product_id)
 
--- src/core/models/product.py
+++ src/core/models/product.py
@@ -1,9 +1,10 @@
-from beanie import Document, Indexed
 import pymongo
-from core.schemas.category import Category
-from uuid import UUID, uuid4
 from pydantic import Field
 from typing import Optional
+from uuid import UUID, uuid4
+from beanie import Document, Indexed
+from core.schemas.category import Category
+from loguru import logger
 
 class Product(Document):
     id: UUID = Field(default_factory=uuid4)
@@ -20,3 +21,52 @@ class Product(Document):
                 ("description", pymongo.TEXT),
             ]
         ]
+
+    async def create_product(self):
+        if not self.name or not self.price or not self.category:
+            logger.error(f"Missing required fields: name, price, or category")
+            raise ValueError("Missing required fields: name, price, or category")
+        try:
+            res = await self.insert()
+            logger.info(f"Created product: {res}")
+            return res
+        except Exception as e:
+            logger.error(e)
+            raise e
+    
+
+    @classmethod
+    async def get_products(cls):
+        try:
+            res = await cls.find_all().to_list()
+            logger.info(f"Found {len(res)} products")
+            return res
+        except Exception as e:
+            logger.error(e)
+            return []
+
+    @classmethod
+    async def get_product(cls, product_id: UUID):
+        if not isinstance(product_id, UUID):
+            logger.error(f"Invalid product_id: {product_id}")
+            raise ValueError("Invalid product_id")
+        try:
+            res = await cls.find_one(product_id)
+            logger.info(f"Found product: {res}")
+            return res
+        except Exception as e:
+            logger.error(e)
+            return None
+
+    @classmethod
+    async def delete_product(cls, product_id: UUID):
+        if not isinstance(product_id, UUID):
+            logger.error(f"Invalid product_id: {product_id}")
+            raise ValueError("Invalid product_id")
+        try:
+            res = await cls.find_one(product_id).delete()
+            logger.info(f"Deleted product: {res}")
+            return res
+        except Exception as e:
+            logger.error(e)
+            return None
